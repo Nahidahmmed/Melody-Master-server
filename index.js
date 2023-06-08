bb1d78edda07b4396cbb31588e5d86d6be617d7d
Nahidahmmed <nahidahmmed411@gmail.com>
2023-06-08 16:56:33 +0600

    popular classes data

--- index.js
+++ index.js
@@ -8,6 +8,60 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ht72zna.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const classesCollection = client.db("AllData").collection("classes");
+    const instructorCollection = client.db("AllData").collection("instructor");
+
+    app.get('/PopularClasses', async(req,res)=>{
+        const query = {}
+        const cursor = classesCollection.find(query).sort({studentsNumber: -1 }).limit(6)
+        const result = await cursor.toArray();
+        res.send(result);
+        
+    })
+    app.get('/PopularInstructor', async(req,res)=>{
+        const query = {}
+        const cursor = instructorCollection.find(query).sort({numStudents: -1 }).limit(6)
+        const result = await cursor.toArray();
+        res.send(result);
+        
+    })
+
+    // app.get('/classes', async(req,res)=>{
+    //     const result = await classesCollection.find().toArray()
+    //     res.send(result)
+    // })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.get('/',(req,res) =>{
     res.send('assignment 12 is running')
 });
