0c3b0c8fb002f6d845ebe86dba2ccbd6cdbe5101
Nahidahmmed <nahidahmmed411@gmail.com>
2023-06-10 21:12:02 +0600

    make admin

--- index.js
+++ index.js
@@ -57,15 +57,27 @@ async function run() {
     })
 
     // users
-    app.get('/users',async(req,res) =>{
+    app.get('/users', async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     })
-    app.put('/users/:email', async (req,res) =>{
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await usersCollection.updateOne(filter,updateDoc);
+      res.send(result)
+    })
+   
+    app.put('/users/:email', async (req, res) => {
       const email = req.params.email;
       const user = req.body;
-      const query = {email:email};
-      const options = {upsert: true};
+      const query = { email: email };
+      const options = { upsert: true };
       const updateDoc = {
         $set: user,
       }
