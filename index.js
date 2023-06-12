26ff276ff9e630ed808751abcfca4d3ebf14f13e
Nahidahmmed <nahidahmmed411@gmail.com>
2023-06-12 21:33:16 +0600

    deploye

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY);
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -28,8 +29,17 @@ const verifyJWT = (req, res, next) => {
   })
 }
 
+const verifyAdmin = async (req,res, next) =>{
+  const email = req.decoded.email;
+  const query = {email: email}
+  const user = await usersCollection.findOne(query);
+  if (user?.role !== 'admin'){
+    return res.status(401).send({ error: true, message: 'unauthorized access' })
+  }
+  next();
+}
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ht72zna.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -44,7 +54,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    
 
     const classesCollection = client.db("AllData").collection("classes");
     const instructorCollection = client.db("AllData").collection("instructor");
@@ -52,21 +62,56 @@ async function run() {
     const usersCollection = client.db("AllData").collection("users");
 
 
-    app.post('/jwt', (req, res) => {
-      const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' })
-      res.send({ token })
-    })
+    // get apis
 
-    const verifyAdmin = async (req,res, next) =>{
-      const email = req.decoded.email;
+
+    app.get('/user/admin/:email',verifyJWT, async(req,res) =>{
+      const email = req.params.email;
+
+      if(req.decoded.email !== email){
+        return res.send({admin: false})
+      }
+
       const query = {email: email}
       const user = await usersCollection.findOne(query);
-      if (user?.role !== 'admin'){
-        return res.status(401).send({ error: true, message: 'unauthorized access' })
+      const result = {admin: user?.role === 'admin'}
+      res.send(result)
+    })
+    app.get('/user/instructor/:email',verifyJWT, async(req,res) =>{
+      const email = req.params.email;
+
+      if(req.decoded.email !== email){
+        return res.send({admin: false})
       }
-      next();
-    }
+
+      const query = {email: email}
+      const user = await usersCollection.findOne(query);
+      const result = {admin: user?.role === 'instructor'}
+      res.send(result)
+    })
+
+    app.get('/carts',verifyJWT,  async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        return res.send([]);
+      }
+
+      const decodedEmail = req.decoded.email;
+      if(email !== decodedEmail){
+        return res.status(403).send({ error: true, message: 'unauthorized access' })
+      }
+
+      const query = { email: email };
+      const result = await cartsCollection.find(query).toArray();
+      res.send(result);
+    })
+   // users
+   app.get('/users',verifyJWT,verifyAdmin, async (req, res) => {
+    const result = await usersCollection.find().toArray();
+    res.send(result);
+  })
+
+    
 
     app.get('/PopularClasses', async (req, res) => {
       const query = {}
@@ -91,6 +136,20 @@ async function run() {
       const result = await classesCollection.find().toArray()
       res.send(result)
     })
+    
+// End -------------------------
+
+    // other apis
+
+   
+
+
+    // cart collection 
+
+   
+
+
+
     app.post('/classes', async (req, res) => {
       const newItem = req.body;
       const result = await classesCollection.insertOne(newItem);
@@ -121,12 +180,14 @@ async function run() {
       res.send(result)
     })
 
-
-    // users
-    app.get('/users',verifyJWT,verifyAdmin, async (req, res) => {
-      const result = await usersCollection.find().toArray();
-      res.send(result);
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' })
+      res.send({ token })
     })
+    //  END -------------------
+
+    
     app.patch('/users/admin/:id', async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
@@ -163,50 +224,7 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/user/admin/:email',verifyJWT, async(req,res) =>{
-      const email = req.params.email;
-
-      if(req.decoded.email !== email){
-        return res.send({admin: false})
-      }
-
-      const query = {email: email}
-      const user = await usersCollection.findOne(query);
-      const result = {admin: user?.role === 'admin'}
-      res.send(result)
-    })
-    app.get('/user/instructor/:email',verifyJWT, async(req,res) =>{
-      const email = req.params.email;
-
-      if(req.decoded.email !== email){
-        return res.send({admin: false})
-      }
-
-      const query = {email: email}
-      const user = await usersCollection.findOne(query);
-      const result = {admin: user?.role === 'instructor'}
-      res.send(result)
-    })
-
-
-    // cart collection 
-
-    app.get('/carts',verifyJWT,  async (req, res) => {
-      const email = req.query.email;
-      if (!email) {
-        return res.send([]);
-      }
-
-      const decodedEmail = req.decoded.email;
-      if(email !== decodedEmail){
-        return res.status(403).send({ error: true, message: 'unauthorized access' })
-      }
-
-      const query = { email: email };
-      const result = await cartsCollection.find(query).toArray();
-      res.send(result);
-    })
-
+    
     app.post('/carts', async (req, res) => {
       const item = req.body;
       const result = await cartsCollection.insertOne(item);
@@ -225,11 +243,11 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+   client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    // await client.close();
+    await client.close();
   }
 }
 run().catch(console.dir);
