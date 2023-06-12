dfd77d5f978acd4cf6b7a262719885d5a251fce8
Nahidahmmed <nahidahmmed411@gmail.com>
2023-06-12 17:24:06 +0600

    is admin

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-const  jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
@@ -10,6 +10,23 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' })
+  }
+  // brr token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+
+    req.decoded = decoded;
+    next();
+  })
+}
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -35,10 +52,10 @@ async function run() {
     const usersCollection = client.db("AllData").collection("users");
 
 
-    app.post('/jwt' , (req,res) =>{
+    app.post('/jwt', (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' })
-      res.send({token})
+      res.send({ token })
     })
 
     app.get('/PopularClasses', async (req, res) => {
@@ -64,7 +81,7 @@ async function run() {
       const result = await classesCollection.find().toArray()
       res.send(result)
     })
-    app.post('/classes', async(req,res) => {
+    app.post('/classes',verifyJWT, async (req, res) => {
       const newItem = req.body;
       const result = await classesCollection.insertOne(newItem);
       res.send(result);
@@ -78,10 +95,10 @@ async function run() {
           status: 'approve'
         }
       }
-      const result = await classesCollection.updateOne(filter,updateDoc);
+      const result = await classesCollection.updateOne(filter, updateDoc);
       res.send(result)
     })
-    
+
     app.patch('/classes/denied/:id', async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
@@ -90,13 +107,13 @@ async function run() {
           status: 'denied'
         }
       }
-      const result = await classesCollection.updateOne(filter,updateDoc);
+      const result = await classesCollection.updateOne(filter, updateDoc);
       res.send(result)
     })
 
 
     // users
-    app.get('/users', async (req, res) => {
+    app.get('/users',verifyJWT, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     })
@@ -108,7 +125,7 @@ async function run() {
           role: 'admin'
         }
       }
-      const result = await usersCollection.updateOne(filter,updateDoc);
+      const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result)
     })
     app.patch('/users/instructor/:id', async (req, res) => {
@@ -119,10 +136,10 @@ async function run() {
           role: 'instructor'
         }
       }
-      const result = await usersCollection.updateOne(filter,updateDoc);
+      const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result)
     })
-   
+
     app.put('/users/:email', async (req, res) => {
       const email = req.params.email;
       const user = req.body;
@@ -136,14 +153,45 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/user/admin/:email',verifyJWT, async(req,res) =>{
+      const email = req.params.email;
+
+      if(req.decoded.email !== email){
+        res.send({admin: false})
+      }
+
+      const query = {email: email}
+      const user = await usersCollection.findOne(query);
+      const result = {admin: user?.role === 'admin'}
+      res.send(result)
+    })
+    app.get('/user/instructor/:email',verifyJWT, async(req,res) =>{
+      const email = req.params.email;
+
+      if(req.decoded.email !== email){
+        res.send({admin: false})
+      }
+
+      const query = {email: email}
+      const user = await usersCollection.findOne(query);
+      const result = {admin: user?.role === 'instructor'}
+      res.send(result)
+    })
+
 
     // cart collection 
 
-    app.get('/carts', async (req, res) => {
+    app.get('/carts', verifyJWT, async (req, res) => {
       const email = req.query.email;
       if (!email) {
         res.send([]);
       }
+
+      const decodedEmail = req.decoded.email;
+      if(email !== decodedEmail){
+        return res.status(403).send({ error: true, message: 'unauthorized access' })
+      }
+
       const query = { email: email };
       const result = await cartsCollection.find(query).toArray();
       res.send(result);
@@ -163,9 +211,9 @@ async function run() {
     })
 
     // create payment 
-    app.post('/create-payment-intent', async(req,res) => {
-      const {price} = req.body;
-      const amount = price*100;
+    app.post('/create-payment-intent', async (req, res) => {
+      const { price } = req.body;
+      const amount = price * 100;
       const paymentIntent = await stripe.paymentIntent.create({
         amount: amount,
         currency: 'usd',
